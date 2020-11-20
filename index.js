29c0dfdab0d6b62678123185d12c423871476712
ritikpy <ritikvarshney2014@gmail.com>
2020-11-20 10:11:15 +0530

    --update: routing done

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+require("dotenv").config();
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const cors = require('cors');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
+require('./config/dbconnection');
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
-const connectionString = 'mongodb+srv://ritikv:ritik@12345@cluster0.l20g4.mongodb.net/<dbname>?retryWrites=true&w=majority';
+app.use('/api/user', require('./router/api/user'));
 
-mongoose.connect(connectionString, {
- useNewUrlParser: true,
- useUnifiedTopology: true
-})
- .then(() => app.listen(PORT, ()=> console.log(`server running on port: ${PORT}`)))
- .catch( error => console.log(error.message));
- mongoose.set('useFindAndModify', false);
+app.listen(PORT, (err) => {
+ if (err) {
+     console.log("Error in running server");
+     return;
+ }
+ console.log(`Server is up and running on port ${PORT}`);
+});
